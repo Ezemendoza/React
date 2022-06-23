78f51aaea73a80efed70cc2cb0ad636744079a06
ezequielmendoza <ezequielmendoza99@gmail.com>
2022-06-22 21:03:44 -0300

    subida

--- src/navBar/CartWidget.js
+++ src/navBar/CartWidget.js
@@ -3,8 +3,12 @@ import carrito from '../carrito.png'
 import { CartContext } from '../CartContext/CartContext'
 
 const CartWidget = () => {
-const{cart, totalPrecio, carritoVacio, removeItem} = useContext(CartContext)
-    return <img src={carrito}/>
- }
+const{cart} = useContext(CartContext)
+    return (
+   <div><img src={carrito}/><span className='contador-carrito'>{cart.length}</span></div>
+
+   
+        
+ )}
 
 export default CartWidget
--- src/navBar/navBar.js
+++ src/navBar/navBar.js
@@ -8,7 +8,7 @@ import { CartContext } from '../CartContext/CartContext';
 
 const Menu = () => {
     const{cart, totalPrecio, carritoVacio, removeItem} = useContext(CartContext)
-    console.log(cart.length)
+   console.log(cart)
     return <nav className="listaNavbar">
         
         <Link to={"/"} className="mt-3 "><img src={logo} className="App-logo" alt="logo" /></Link>
@@ -17,7 +17,8 @@ const Menu = () => {
     <Link to={"/contacto"} className="mt-3 palabra__lista">Contacto</Link>
     <Link to={"/ubicacion"} className="mt-3 palabra__lista">Ubicacion</Link>
 
-    <Link to={"/cart"} className="mt-3 contenedor-carrito "><li className='mr-4'> <CartWidget/>   <span className='contador-carrito'>{cart.length}</span></li></Link>
+    <Link to={"/cart"} className="mt-3 contenedor-carrito "><li className='mr-4'> 
+    { (cart.length!==0) && <CartWidget/>  }</li></Link>
  
 </ul>
 </nav>
